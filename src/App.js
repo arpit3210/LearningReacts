8861fb7d77e4e038a469cef4b497d51705ac9c71
Arpit <arpitsinghthakur321@gmail.com>
2022-10-10 13:04:57 +0530

    adding normal javascript logic to components

--- src/App.js
+++ src/App.js
@@ -6,25 +6,26 @@ function App() {
   const expense=[
     {date: new Date(2022, 8, 9),
      title: "Car Insurance",
-     amount: 234.34
+     amount: 34.34
     },
     {date: new Date(2022, 8, 9),
-      title: "Car Insurance",
-      amount: 234.34
+      title: "Child fees",
+      amount: 854.34
      },
      {date: new Date(2022, 8, 9),
-      title: "Car Insurance",
-      amount: 234.34
+      title: "Room rent",
+      amount: 82734.34
      },
      {date: new Date(2022, 8, 9),
-      title: "Car Insurance",
-      amount: 234.34
+      title: "Travel charges",
+      amount: 393.34
      },
   ]
   return ( <div>
    <ExpenseItem  date={expense[0].date} title={expense[0].title} amount={expense[0].amount} ></ExpenseItem>
    <ExpenseItem  date={expense[1].date} title={expense[1].title} amount={expense[1].amount} ></ExpenseItem>
-   <ExpenseItem  date={expense[0].date} title={expense[2].title} amount={expense[2].amount} ></ExpenseItem>
+   <ExpenseItem  date={expense[2].date} title={expense[2].title} amount={expense[2].amount} ></ExpenseItem>
+   <ExpenseItem  date={expense[3].date} title={expense[3].title} amount={expense[3].amount} ></ExpenseItem>
    </div>
   );
 }
